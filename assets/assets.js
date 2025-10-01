4fb185b0d15f0f71f9d6a62a6333bb8627acf848
poojajaipalgaikwad <poojajaipalgaikwad116@gmail.com>
2025-10-01 18:42:17 +0530

    Todays changes

--- assets/assets.js
+++ assets/assets.js
@@ -3,11 +3,15 @@ import HeaderBgColour from './header-bg-color.png'
 import MenuBlack from './menu-black.png'
 import MoonIcon from './moon_icon.png'
 import CloseBlack from './close-black.png'
+import profile from './profile-img.png'
+import homeopathy from './homeopathy1.jpg'
 export const assets = {
     Logo : Logo,
     HeaderBgColour : HeaderBgColour,
     MenuBlack : MenuBlack,
     MoonIcon :MoonIcon,
-    CloseBlack : CloseBlack
+    CloseBlack : CloseBlack,
+    Profile : profile,
+    Homeopathy : homeopathy
 
 }
